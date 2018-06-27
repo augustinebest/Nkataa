1e7e4f29d1788accffef34d05b6330b062867227
augustinebest <augustusbest00@gmail.com>
2018-06-27 08:48:25 +0100

    created the add post function that creates the given post of a user

---
+++ Controllers/PostController.js
@@ -0,0 +1,16 @@
+var model = require('../Model/Post');
+
+exports.addPost = function(req, res) {
+    var data = {
+        time : Date.now(),
+        postBody : req.body.postBody,
+        user : req.body.user
+    };
+    model.create(data, function(err){
+        if(err) {
+            res.json({err: err, message: 'Post could not be created'});
+        } else {
+            res.json({message: 'Post was created Successfully!'});
+        }
+    })
+}
